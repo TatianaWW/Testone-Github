ea89b4ab125e15c83a6a23e59722edd1b4776d03
l_ser <l_ser@LAPTOP-VB9FNC1V>
2020-05-21 19:16:20 -0500

    Github

--- Testone Github/src/CodeOneOne.java
+++ Testone Github/src/CodeOneOne.java
@@ -3,7 +3,7 @@ public class CodeOneOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("CodeOdeOne   g");
+System.out.println("CodeOdeOne.github");
 	}
 
 }
